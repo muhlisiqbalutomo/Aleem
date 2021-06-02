d4055e4bbe43546a0e14d54051b3cc4e0ef23909
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2021-06-02 08:41:49 +0700

    Slicing Doctor Page - Horizontal Scroll Category Doctor & Section Label

--- src/components/molecules/BabKonsultasi/index.js
+++ src/components/molecules/BabKonsultasi/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {ILBabAkidah} from '../../../assets';
+import {colors, fonts} from '../../../utils';
 
 const BabKonsultasi = () => {
   return (
-    <View>
-      <Text>Bab Konsultasi Component</Text>
+    <View style={styles.container}>
+      <ILBabAkidah style={styles.illustration} />
+      <Text style={styles.label}>Akidah</Text>
     </View>
   );
 };
 
 export default BabKonsultasi;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 12,
+    backgroundColor: colors.secondary,
+    // alignSelf: 'flex-start',
+    width: 170,
+    height: 130,
+    borderRadius: 10,
+    marginRight: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  illustration: {
+    marginRight: 10,
+  },
+  label: {
+    fontSize: 14,
+    fontFamily: fonts.primary[800],
+    color: colors.white,
+    textAlign: 'center',
+  },
+});
--- src/pages/Ustadz/index.js
+++ src/pages/Ustadz/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Gap} from '../../components/atoms';
 import {BabKonsultasi, NewsItem, RatedUstadz} from '../../components/molecules';
 import HomeProfile from '../../components/molecules/HomeProfile';
 import {colors, fonts} from '../../utils';
@@ -7,23 +8,33 @@ import {colors, fonts} from '../../utils';
 const Ustadz = () => {
   return (
     <View style={styles.page}>
-      <HomeProfile />
-      <Text style={styles.welcome}>
-        Assalamualaikum, {'\n'}konsultasi apa hari ini?
-      </Text>
-      <BabKonsultasi />
-      <BabKonsultasi />
-      <BabKonsultasi />
-      <BabKonsultasi />
-      <BabKonsultasi />
-      <Text>Top Rated Ustadz</Text>
-      <RatedUstadz />
-      <RatedUstadz />
-      <RatedUstadz />
-      <Text>Good News</Text>
-      <NewsItem />
-      <NewsItem />
-      <NewsItem />
+      <View style={styles.content}>
+        <HomeProfile />
+        <Text style={styles.welcome}>
+          Assalamualaikum, {'\n'}konsultasi apa hari ini?
+        </Text>
+        <View style={styles.wrapperScroll}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <View style={styles.konsultasi}>
+              <Gap width={16} />
+              <BabKonsultasi />
+              <BabKonsultasi />
+              <BabKonsultasi />
+              <BabKonsultasi />
+              <BabKonsultasi />
+              <Gap width={6} />
+            </View>
+          </ScrollView>
+        </View>
+        <Text style={styles.sectionLabel}>Top Rated Ustadz</Text>
+        <RatedUstadz />
+        <RatedUstadz />
+        <RatedUstadz />
+        <Text style={styles.sectionLabel}>Good News</Text>
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+      </View>
     </View>
   );
 };
@@ -32,11 +43,32 @@ export default Ustadz;
 
 const styles = StyleSheet.create({
   page: {
+    backgroundColor: colors.bottom,
+    flex: 1,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
     paddingVertical: 30,
     paddingHorizontal: 16,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
   welcome: {
     fontSize: 20,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 30,
+    marginBottom: 16,
+  },
+  wrapperScroll: {
+    marginHorizontal: -16,
+  },
+  konsultasi: {
+    flexDirection: 'row',
+  },
+  sectionLabel: {
+    fontSize: 16,
     fontFamily: fonts.primary[600],
     color: colors.text.primary,
     marginTop: 30,
