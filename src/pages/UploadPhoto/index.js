911f96216662021787a6cb0fdd941b1df01acdf4
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2021-06-22 23:46:36 +0700

    Mengirimkan parameter ke halaman lain

--- src/pages/UploadPhoto/index.js
+++ src/pages/UploadPhoto/index.js
@@ -6,7 +6,9 @@ import {Button, Gap, Header, Link} from '../../components';
 import {colors, fonts} from '../../utils';
 import {showMessage} from 'react-native-flash-message';
 
-const UploadPhoto = ({navigation}) => {
+const UploadPhoto = ({navigation, route}) => {
+  const {fullName, profession} = route.params;
+
   const [hasPhoto, setHasPhoto] = useState(false);
   const [photo, setPhoto] = useState(ILNullPhoto);
 
@@ -37,8 +39,8 @@ const UploadPhoto = ({navigation}) => {
             {hasPhoto && <IconRemovePhoto style={styles.addPhoto} />}
             {!hasPhoto && <IconAddPhoto style={styles.addPhoto} />}
           </TouchableOpacity>
-          <Text style={styles.name}>Muhlis Iqbal Utomo</Text>
-          <Text style={styles.profession}>CEO & Founder of iqbalworks</Text>
+          <Text style={styles.name}>{fullName}</Text>
+          <Text style={styles.profession}>{profession}</Text>
         </View>
         <View>
           <Button
