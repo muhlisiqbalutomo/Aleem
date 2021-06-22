32b369df517fc8a7b1dcf689527c9cf1886f1ef4
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2021-06-23 03:42:04 +0700

    Profile detail page

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
-import {DummyUser, DummyUstadz3} from '../../assets';
+import {ILNullPhoto} from '../../assets';
 import {Gap, Header, List, Profile} from '../../components';
-import {colors} from '../../utils';
+import {colors, getData} from '../../utils';
 
 const UserProfile = ({navigation}) => {
+  const [profile, setProfile] = useState({
+    fullName: '',
+    profession: '',
+    photo: ILNullPhoto,
+  });
+  useEffect(() => {
+    getData('user').then(res => {
+      const data = res;
+      data.photo = {uri: res.photo};
+      setProfile(data);
+    });
+  }, []);
   return (
     <View style={styles.page}>
       <Header title="Profile" onPress={() => navigation.goBack()} />
       <Gap height={30} />
-      <Profile name="Muhlis Iqbal Utomo" desc="CEO & Founder at iqbalworks" />
+      {profile.fullName.length > 0 && (
+        <Profile
+          name={profile.fullName}
+          desc={profile.profession}
+          photo={profile.photo}
+        />
+      )}
+
       <List
         name="Edit Profile"
         desc="Last Update Yesterday"
@@ -18,19 +37,19 @@ const UserProfile = ({navigation}) => {
         onPress={() => navigation.navigate('UpdateProfile')}
       />
       <List
-        name="Edit Profile"
+        name="Languange"
         desc="Last Update Yesterday"
         type="next"
         icon="language"
       />
       <List
-        name="Edit Profile"
+        name="Give Us Rate"
         desc="Last Update Yesterday"
         type="next"
         icon="rate-app"
       />
       <List
-        name="Edit Profile"
+        name="Help Center"
         desc="Last Update Yesterday"
         type="next"
         icon="help-center"
