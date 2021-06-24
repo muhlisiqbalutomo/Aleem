87bf4e9f910f0778ee4b133bbe42fd3c566b888f
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2021-06-24 08:50:59 +0700

    Menambahkan fungsi logout pada aplikasi

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -3,6 +3,8 @@ import {Image, StyleSheet, Text, View} from 'react-native';
 import {ILNullPhoto} from '../../assets';
 import {Gap, Header, List, Profile} from '../../components';
 import {colors, getData} from '../../utils';
+import {Fire} from '../../config';
+import {showMessage} from 'react-native-flash-message';
 
 const UserProfile = ({navigation}) => {
   const [profile, setProfile] = useState({
@@ -17,6 +19,24 @@ const UserProfile = ({navigation}) => {
       setProfile(data);
     });
   }, []);
+
+  const signOut = () => {
+    Fire.auth()
+      .signOut()
+      .then(() => {
+        console.log('success signout');
+        navigation.replace('GetStarted');
+      })
+      .catch(err => {
+        showMessage({
+          message: err.message,
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
+      });
+  };
+
   return (
     <View style={styles.page}>
       <Header title="Profile" onPress={() => navigation.goBack()} />
@@ -49,10 +69,11 @@ const UserProfile = ({navigation}) => {
         icon="rate-app"
       />
       <List
-        name="Help Center"
+        name="Sign Out"
         desc="Last Update Yesterday"
         type="next"
         icon="help-center"
+        onPress={signOut}
       />
     </View>
   );
