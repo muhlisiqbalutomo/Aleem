f48fea3849910efbec6592fcbc94480667186d05
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2021-06-24 16:14:50 +0700

    Memperbaiki bug pada proses pengecekan login session dihalaman flash screen

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -6,18 +6,20 @@ import {Fire} from '../../config';
 
 const Splash = ({navigation}) => {
   useEffect(() => {
-    setTimeout(() => {
-      Fire.auth().onAuthStateChanged(user => {
+    const unsubscribe = Fire.auth().onAuthStateChanged(user => {
+      setTimeout(() => {
         if (user) {
-          // user lagi login
-          'user: ', user;
           navigation.replace('MainApp');
         } else {
           // user logout
           navigation.replace('GetStarted');
         }
-      });
-    }, 3000);
+      }, 3000);
+    });
+
+    // fungsi untuk membersihkan tumpukan halaman ketika sudah dipanggil,
+    // jadi tidak saling tumpang tindih
+    return () => unsubscribe();
   }, [navigation]);
   return (
     <View style={styles.page}>
